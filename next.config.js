c65764f612fbb4358242e76d42eb193731b7c991
Milad Heydari <m.heydari@rashasoft.ir>
2018-04-18 15:13:01 +0430

    :tada: first initial

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 const path = require("path");
 
 // Uncomment the following and update with all your possible urls to enable the exporting feature
-// exports.exportPathMap = () => ({
-//   "/": { page: "/" },
-//   "/about": { page: "/about" },
-//   "/product/plant": { page: "/product", query: { slug: "plant" } },
-//   "/product/tree": { page: "/product", query: { slug: "tree" } },
-// });
+exports.exportPathMap = () => ({
+  "/": { page: "/" },
+});
 
 exports.webpack = config => {
   // Perform customizations to webpack config
